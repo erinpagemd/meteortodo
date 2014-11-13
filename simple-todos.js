5bb89e2f6b6db35610e0c1eebcadb0781ff44522
erin <erinpagemd@yahoo.com>
2014-11-12 22:39:27 -0600

    sort the returned tasks

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,28 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
-
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is the second task"},
-      {text: "And here is the third task"}
-    ]
+    tasks: function(){
+      //show newest task first
+      return Tasks.find({}, {sort: {createdAt:-1}});
+    }
   })
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //clear form
+      event.target.text.value = "";
+
+      //prevent default form submit
+      return false;
+    }
+  });
 }
