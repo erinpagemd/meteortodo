93d54470825abd29f4ee305cb11d7f085b06814b
YOUR NAME <erinpagemd@yahoo.com>
2014-12-09 10:55:51 -0600

    some changes

--- simple-todos.js
+++ simple-todos.js
@@ -44,6 +44,7 @@ if (Meteor.isClient) {
       //set the checked property to the opposite of its current value
       Meteor.call("setChecked", this._id, ! this.checked);
     },
+    
     "click .delete": function(){
       Meteor.call("deleteTask", this._id);
     }
